c1166162dd09977246bed9036c908843beac71bf
Krzysztof_Rog <asddjasdk@gmail.com>
2025-10-10 09:39:28 +0200

    lab 1

---
+++ lab1/Application/Domain/Sex.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Contacts.Application.Domain
+
+{
+    public class Sex
+    {
+
+    }
+    
+}
