e2fe86e443c8ac70f85df37badfa2a804fc7a062
Jakub Grzybowski <jakub@grzbowski.pl>
2025-10-10 09:57:24 +0200

    lab1,strona 14 - dokoncz

---
+++ Lab1/Application/Services/CategoryServices.cs
@@ -0,0 +1,33 @@
+
+using Contacts.Application.UseCases;
+using Contacts.Application.UseCases.DTOs;
+using Contacts.Application.Repositories;
+
+namespace Contacts.Application.Services;
+
+public class CategoryServices(
+ICategoryRepository categoryRepository, IUnitOfWork unitOfWork
+) : ICategoryUseCases
+{
+    public void AddCategory(AddCategoryDTO categoryDTO)
+    {
+        if (categoryRepository.GetCategoryByName(categoryDTO.Name) == null)
+        {
+            categoryRepository.Add(categoryDTO); unitOfWork.Save();
+        }
+    }
+else
+{
+}
+throw new ServiceException("Kategoria_o_tej_nazwie_już_istnieje");
+    public void RemoveCategory(int categoryID)
+    {
+        var category = categoryRepository.GetCategory(categoryID); if (category != null)
+        {
+            categoryRepository.RemoveCategory(category);
+            unitOfWork.Save();
+        }
+        else
+        {
+            throw new ServiceException(
+            $"Kategoria_id:_{categoryID}_nie_istnieje");
---
+++ Lab1/Application/UseCases/ICategoryUseCases.cs
@@ -0,0 +1,7 @@
+using Contacts.Application.UseCases.DTOs;
+namespace Contacts.Application.UseCases;
+
+public interface ICategoryUseCases {
+    AddCategoryResult AddCategory(AddCategoryDTO addCategoryDTO);
+    void RemoveCategory(int categoryId); 
+}
