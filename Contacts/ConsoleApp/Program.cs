af1acb09cd821ce77678b81eed1866c80f40849a
Michal Zultaszek <yel@yel.yel>
2025-10-15 15:45:24 +0200

    lab1 dok2

--- Contacts/ConsoleApp/Program.cs
+++ Contacts/ConsoleApp/Program.cs
@@ -25,7 +25,8 @@ context.Database.EnsureCreated();
 var categoryRepository = new CategoryRepository(context);
 var unitOfWork = new UnitOfWork(context);
 var categoryServices = new CategoryServices(categoryRepository, unitOfWork);
-categoryServices.RemoveCategory(1);
+categoryServices.AddCategory(new AddCategoryDTO { Name = "nowa kategoria 3"});
+//categoryServices.RemoveCategory(1);
 foreach (var category in context.Categories)
 {
     Console.WriteLine(category.Name);
