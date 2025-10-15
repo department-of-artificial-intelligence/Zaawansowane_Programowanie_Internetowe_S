5655abef96ec190d299a9a097f591ef38f292aa5
xx <gfgdgf@email.emaik>
2025-10-15 15:45:36 +0200

    xxx

---
+++ lab/Application/Repositories/ICategoryUseCases.cs
@@ -0,0 +1,8 @@
+using Contacts.Application.UseCases.DTOs; 
+namespace Contacts.Application.UseCases;
+
+public interface ICategoryUseCases
+{
+    AddCategoryResult AddCategory(AddCategoryDTO addCategoryDTO);
+    void RemoveCategory(int categoryId); 
+}
