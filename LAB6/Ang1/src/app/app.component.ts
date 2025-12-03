12d7c75a5b787c0547864b89b4b1425ea013232a
Tomasz Bekus <tb@smth.smth>
2025-12-03 15:43:53 +0100

    Lab 6 - Ok

--- LAB6/Ang1/src/app/app.component.ts
+++ LAB6/Ang1/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: '<app-first></app-first>',
+  standalone: false,
   styles: [],
 })
 export class AppComponent {}
--- LAB6/Ang1/src/app/first/first.component.ts
+++ LAB6/Ang1/src/app/first/first.component.ts
@@ -3,9 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-first',
   templateUrl: 'first.component.html',
-  styles: '',
+  standalone: false,
+  styleUrl: 'first.component.css',
 })
 export class FirstComponent implements OnInit {
+  title: string = 'Pierwsza wartość przekazana z komponentu';
+  person = {
+    firstName: 'Ala',
+    lastName: 'Nowak',
+  };
+  h1Class?: string;
+
   constructor() {}
+
+  metoda() {
+    return 'Wartośc zwrócona z metody';
+  }
   ngOnInit(): void {}
+
+  onClick() {
+    this.title = 'Po kliknieciu';
+    this.h1Class = 'Boo';
+  }
 }
