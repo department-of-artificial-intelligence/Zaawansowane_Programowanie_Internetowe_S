3a6f7c18bff748736a7dafdef9f0a2eafa8301d8
Tomasz Bekus <tb@smth.smth>
2025-12-03 15:20:56 +0100

    Lab 6 - Standalone error

---
+++ LAB6/Ang1/src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<app-first></app-first>',
+  styles: [],
+})
+export class AppComponent {}
---
+++ LAB6/Ang1/src/app/first/first.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first',
+  templateUrl: 'first.component.html',
+  styles: '',
+})
+export class FirstComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {}
+}
