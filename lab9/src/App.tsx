2fdeb4a41cd71ace239c7461c9aa2fa16f968cbc
jan kowlaczyk <kami@dere.pl>
2026-01-14 15:37:33 +0100

    lab9

---
+++ lab9/src/App.tsx
@@ -0,0 +1,12 @@
+import First from './First.tsx'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <First />
+    </>
+  )
+}
+
+export default App
---
+++ lab9/src/First.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export class First extends React.Component {
+    render() {
+        return (
+            <div>Hello world</div>
+        );
+    }
+}
